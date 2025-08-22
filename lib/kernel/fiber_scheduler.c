c83996ed9d4693a50d4dd8220a8e8915e15f4a33
Tapio Nevalainen <tapio.nevalainen@tuni.fi>
2025-08-22 14:24:33 +0300

    Get rid of the fiber scheduler header file

--- lib/kernel/fiber_scheduler.c
+++ lib/kernel/fiber_scheduler.c
@@ -21,8 +21,52 @@
    IN THE SOFTWARE.
 */
 
-#include "fiber_scheduler.h"
+//#include "fiber_scheduler.h"
 #include <stdio.h>
+
+/* Data structure that contains work-group information.
+   Passed in from the kernel. */
+typedef struct
+{
+  unsigned long local_size_x;
+  unsigned long local_size_y;
+  unsigned long local_size_z;
+  unsigned long subgroup_size;
+  /* Number of subgroups in workgroup */
+  unsigned long n_subgroups;
+  /* Number of workitems waiting at barrier */
+  unsigned long waiting_count;
+  /* Number of subgroup barriers that are active. Active subgroup barrier
+     means one of more workitems in a subgroup has reached subgroup barrier. */
+  unsigned long sg_barriers_active;
+  /* Workitem counter for each subgroup. Counts the number of workitems that
+     have reached a barrier (workgroup or subgroup barrier). */
+  unsigned long *sg_wi_counter;
+  /* Subgroup barrier counter for each subgroup. This holds the information
+     whether subgroup is at sugbroup barrier. Can only have values 0 and 1. */
+  unsigned long *sg_barrier_counter;
+} wgState;
+
+void __pocl_fiber_sched_init (wgState *wg_state,
+                              unsigned long *sg_wi_counter,
+                              unsigned long *sg_barrier_counter);
+
+long __pocl_fiber_schedule_work_item (wgState *wg_state);
+
+void __pocl_fiber_wg_barrier_reached (long local_id_x,
+                                      long local_id_y,
+                                      long local_id_z,
+                                      wgState *wg_state);
+
+void __pocl_fiber_sg_barrier_reached (long local_id_x,
+                                      long local_id_y,
+                                      long local_id_z,
+                                      wgState *wg_state);
+
+static void resolve_barriers (wgState *wg_state);
+
+static void print_barrier_status (wgState *wg_state);
+
 
 /* Enable debug prints from the Fiber scheduler. */
 /* #define DEBUG_FIBER_SCHEDULER */
