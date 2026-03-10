887331f3b492e11ee713ec2c0b969951296adb08
Tapio Nevalainen <tapio.nevalainen@tuni.fi>
2026-03-10 17:22:12 +0200

    Add licenses and exit on error

--- examples/GROMACS-ACpp/GROMACS_runner.sh
+++ examples/GROMACS-ACpp/GROMACS_runner.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
+#
+# Copyright (c) 2026 Tapio Nevalainen / Tampere University
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+#
+# Executes GROMACS simulation with given input (Cold run for kernel
+# compilation and proper longer run). Runs python script to produce a JSON that
+# contains execution times of the run.
+
+# Immediately exit with non-zero status.
+set -e
 
 # Path to input
 SAMPLE_DIR=$1
